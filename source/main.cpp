8a45e530ec7e59f6987611a2dc46675c34d6dd0c
smokejohn <johannesrauch91@gmail.com>
2021-01-17 03:33:36 +0100

    Lesson27 - Collision Detection

--- source/main.cpp
+++ source/main.cpp
@@ -9,32 +9,35 @@
 
 // the dot that will move around the screen
 class Dot {
-    public:
-        // the dimensions of the dot
-        static const int DOT_WIDTH = 20;
-        static const int DOT_HEIGHT = 20;
+   public:
+    // the dimensions of the dot
+    static const int DOT_WIDTH = 20;
+    static const int DOT_HEIGHT = 20;
 
-        // maximum axis velocity of the dot
-        static const int DOT_VEL = 10;
+    // maximum axis velocity of the dot
+    static const int DOT_VEL = 10;
 
-        // initalizes the variables
-        Dot();
+    // initalizes the variables
+    Dot();
 
-        // takes key presses and adjusts the dot's velocity
-        void handleEvent(SDL_Event& e);
+    // takes key presses and adjusts the dot's velocity
+    void handleEvent(SDL_Event& e);
 
-        // moves the dot
-        void move();
+    // moves the dot
+    void move(SDL_Rect& wall);
 
-        // shows the dot on the screen
-        void render();
+    // shows the dot on the screen
+    void render();
 
-    private:
-        // the x and y offsets of the dot
-        int mPosX, mPosY;
+   private:
+    // the x and y offsets of the dot
+    int mPosX, mPosY;
 
-        // the velocity of the dot
-        int mVelX, mVelY;
+    // the velocity of the dot
+    int mVelX, mVelY;
+
+    // dot's collision box
+    SDL_Rect mCollider;
 };
 
 // the application time based timer
@@ -120,6 +123,9 @@ bool init();
 bool loadMedia();
 // frees media and shuts down SDL
 void close();
+
+// box collision detector
+bool checkCollision(SDL_Rect a, SDL_Rect b);
 
 // loads individual image as texture
 SDL_Texture* loadTexture(std::string path);
@@ -243,9 +249,15 @@ int main(int argc, char* argv[]) {
             // the dot that will be moving around the screen
             Dot dot;
 
+            // the wall collision object
+            SDL_Rect wall;
+            wall.x = 300;
+            wall.y = 40;
+            wall.w = 40;
+            wall.h = 400;
+
             // while application is running
             while (!quit) {
-
                 // handle events on queue
                 while (SDL_PollEvent(&e) != 0) {
                     // user requests quit
@@ -258,18 +270,22 @@ int main(int argc, char* argv[]) {
                 }
 
                 // move the dot
-                dot.move();
+                dot.move(wall);
 
                 // clear screen
                 SDL_SetRenderDrawColor(gRenderer, 255, 255, 255, 255);
                 SDL_RenderClear(gRenderer);
 
-                // render objects
+                // render wall
+                SDL_SetRenderDrawColor( gRenderer, 0,0,0,255);
+                SDL_RenderDrawRect(gRenderer, &wall);
+
+
+                // render dot
                 dot.render();
 
                 // update the screen
                 SDL_RenderPresent(gRenderer);
-
             }
         }
     }
@@ -505,56 +521,119 @@ Dot::Dot() {
     mPosX = 0;
     mPosY = 0;
 
+    // set collision box dimensions
+    mCollider.w = DOT_WIDTH;
+    mCollider.h = DOT_HEIGHT;
+
     // initalize the velocity
     mVelX = 0;
     mVelY = 0;
 }
 
-void Dot::handleEvent(SDL_Event &e) {
+void Dot::handleEvent(SDL_Event& e) {
     // if a key was pressed
     if (e.type == SDL_KEYDOWN && e.key.repeat == 0) {
         // adjust the velocity
         switch (e.key.keysym.sym) {
-            case SDLK_UP: mVelY -= DOT_VEL; break;
-            case SDLK_DOWN: mVelY += DOT_VEL; break;
-            case SDLK_LEFT: mVelX -= DOT_VEL; break;
-            case SDLK_RIGHT: mVelX += DOT_VEL; break;
+            case SDLK_UP:
+                mVelY -= DOT_VEL;
+                break;
+            case SDLK_DOWN:
+                mVelY += DOT_VEL;
+                break;
+            case SDLK_LEFT:
+                mVelX -= DOT_VEL;
+                break;
+            case SDLK_RIGHT:
+                mVelX += DOT_VEL;
+                break;
         }
     }
 
     // if a key was released
-    else if (e.type== SDL_KEYUP && e.key.repeat == 0) {
+    else if (e.type == SDL_KEYUP && e.key.repeat == 0) {
         // adjust the velocity
         switch (e.key.keysym.sym) {
-            case SDLK_UP: mVelY += DOT_VEL; break;
-            case SDLK_DOWN: mVelY -= DOT_VEL; break;
-            case SDLK_LEFT: mVelX += DOT_VEL; break;
-            case SDLK_RIGHT: mVelX -= DOT_VEL; break;
+            case SDLK_UP:
+                mVelY += DOT_VEL;
+                break;
+            case SDLK_DOWN:
+                mVelY -= DOT_VEL;
+                break;
+            case SDLK_LEFT:
+                mVelX += DOT_VEL;
+                break;
+            case SDLK_RIGHT:
+                mVelX -= DOT_VEL;
+                break;
         }
     }
 }
 
-void Dot::move() {
+void Dot::move(SDL_Rect& wall) {
     // move the dot left or right
     mPosX += mVelX;
+    mCollider.x = mPosX;
 
-    // if the dot went to far to the left or right
-    if ((mPosX < 0) || (mPosX + DOT_WIDTH > SCREEN_WIDTH)) {
+    // if the dot collied or went to far to the left or right
+    if ((mPosX < 0) || (mPosX + DOT_WIDTH > SCREEN_WIDTH) || checkCollision(mCollider, wall)) {
         // move back;
         mPosX -= mVelX;
+        mCollider.x -= mPosX;
     }
 
     // move the dot up or down
     mPosY += mVelY;
+    mCollider.y = mPosY;
 
     // if the dot went to far up or down
-    if ((mPosY < 0) || (mPosY + DOT_HEIGHT > SCREEN_HEIGHT)) {
+    if ((mPosY < 0) || (mPosY + DOT_HEIGHT > SCREEN_HEIGHT) || checkCollision(mCollider, wall)) {
         // move back;
         mPosY -= mVelY;
+        mCollider.y = mPosY;
     }
 }
 
 void Dot::render() {
     // show the dot
     gDotTexture.render(mPosX, mPosY);
+}
+
+bool checkCollision(SDL_Rect a, SDL_Rect b) {
+    // the sides of the rectangles
+    int leftA, leftB;
+    int rightA, rightB;
+    int topA, topB;
+    int bottomA, bottomB;
+
+    // calculate the sides of rect A
+    leftA = a.x;
+    rightA = a.x + a.w;
+    topA = a.y;
+    bottomA = a.y + a.h;
+
+    // calculate the sides of rect A
+    leftB = b.x;
+    rightB = b.x + b.w;
+    topB = b.y;
+    bottomB = b.y + b.h;
+
+    // if any of the sides from A are outside of B
+    if (bottomA <= topB) {
+        return false;
+    }
+    if (topA >= bottomB) {
+        return false;
+    }
+
+    if (rightA <= leftB) {
+        return false;
+    }
+
+    if (leftA >= rightB) {
+        return false;
+    }
+
+    // if none of the sides from A are outside B
+    return true;
 }
